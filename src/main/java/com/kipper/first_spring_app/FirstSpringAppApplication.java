d2b3f6a9250ea818d85fc290d46b6fe3ca107294
Gabriel-V-Martins <gabriel.martins318.dev@gmail.com>
2025-09-30 17:25:23 -0300

    NEW: criacao e configuração do arquivo application-dev.properties

--- src/main/java/com/kipper/first_spring_app/FirstSpringAppApplication.java
+++ src/main/java/com/kipper/first_spring_app/FirstSpringAppApplication.java
@@ -2,8 +2,10 @@ package com.kipper.first_spring_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication // indica que é uma aplicação Spring Boot
+@Profile("dev") // indica que essa configuração é para o perfil de desenvolvimento
 public class FirstSpringAppApplication {
 
 	public static void main(String[] args) {
