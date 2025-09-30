332d028508bff1419a535fedebcffc075df28645
Gabriel-V-Martins <gabriel.martins318.dev@gmail.com>
2025-09-30 17:07:25 -0300

    REFACTOR: adicionando comentários na classe HellowWorldController

--- src/main/java/com/kipper/first_spring_app/controller/HellowWorldController.java
+++ src/main/java/com/kipper/first_spring_app/controller/HellowWorldController.java
@@ -4,10 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/hello-world")
+@RestController // indica que a classe é um controlador REST
+@RequestMapping("/hello-world") // cria um endpoint(caminho) para acessar o método helloWorld
 public class HellowWorldController {
-    @GetMapping
+
+    @GetMapping // mapeia requisicoes do tipo GET para o metodo helloWorld
     public String helloWorld(){
         return "Hello World!";
     }
